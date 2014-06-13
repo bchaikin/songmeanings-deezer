66a9285c280a52a6aca8324d0f0c3913768d14de
Bryan Chaikin <bryan.chaikin@gmail.com>
2014-06-13 09:53:58 -0400

    initial markup commit

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SongMeanings.net</title>
+	<link media="all" rel="stylesheet" href="css/all.css">
+	<script src="js/jquery-1.8.3.min.js"></script>
+	<script src="js/jquery.main.js"></script>
+	<!--[if IE]><script src="js/ie.js"></script><![endif]-->
+</head>
+<body>
+	<div id="wrapper">
+		<header id="header">
+			<div class="header-holder">
+				<strong class="logo"><a href="#">SongMeanings.net</a></strong>
+				<div class="account">
+					<a href="#" class="opener">account</a>
+					<ul>
+						<li><a href="#">Sign Up</a></li>
+						<li class="signin"><a href="#">Login</a></li>
+					</ul>
+				</div>
+			</div>
+		</header>
+		<main id="main" role="main">
+			<section class="album-intro">
+				<div class="image-holder col">
+					<span data-picture data-alt="image description">
+						<span data-src="images/img1.jpg" ></span>
+						<span data-src="images/img1-tablet.jpg" data-media="(max-width:1024px)" ></span>
+						<span data-src="images/img1-tablet2x.jpg" data-media="(max-width:1024px) and (-webkit-min-device-pixel-ratio:1.5), (max-width:1024px) and (min-resolution:144dpi)" ></span>
+						<span data-src="images/img1-mobile.jpg" data-media="(max-width:767px)" ></span>
+						<span data-src="images/img1-mobile2x.jpg" data-media="(max-width:767px) and (-webkit-min-device-pixel-ratio:1.5), (max-width:767px) and (min-resolution:144dpi)" ></span>
+						<!--[if (lt IE 9) & (!IEMobile)]>
+							<span data-src="images/img1.jpg"></span>
+						<![endif]-->
+						<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+						<noscript><img src="images/img1.jpg" width="200" height="200" alt="image description" ></noscript>
+					</span>
+				</div>
+				<div class="info-holder col">
+					<div class="album-info">
+						<dl>
+							<dt>Artist:</dt>
+							<dd><a class="title" href="#">Incubus</a></dd>
+							<dt>Song:</dt>
+							<dd>Anna Molly</dd>
+							<dt>Album:</dt>
+							<dd>Light Grenades</dd>
+						</dl>
+					</div>
+					<div class="follow-block">
+						<a href="#" class="link-follow">Follow <span>Following</span></a>
+					</div>
+				</div>
+				<aside class="info-list col">
+					<p>
+						A cloud hangs over this city by the sea. <br>
+						I watch the ships pass and wonder if she might be out there and sober as well from loneliness. <br>
+						Please do persist, girl it's time we met and made a mess.
+					</p>
+					<div class="text-block hidden-tablet">
+						<p>
+							I picture your face at the back of my eyes. <br>
+							A fire in the attic, proof of the prize! <br>
+							<span class="hidden-mobile">Anna-Molly, Anna-Molly, Anna-Molly.</span>
+						</p>
+					</div>
+					<div class="link-holder"><a href="#">View Full Lyrics</a></div>
+				</aside>
+			</section>
+			<div id="content">
+				<footer class="meta-block">
+					<div class="meta-frame">
+						<div class="meta-holder">
+							<a href="#" class="btn-add">Add Your Thoughts</a>
+							<strong class="title">138 Comments</strong>
+							<div class="filter">
+								<a class="filter-opener" href="#">Filter <span>by Category</span></a>
+							</div>
+							<div class="sort">
+								<a href="#" class="sort-opener">Sort</a>
+								<ul>
+									<li><a href="#">Highest Rated</a></li>
+									<li><a href="#">Oldest First</a></li>
+									<li class="active"><a href="#">Newest First</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="slide-holder">
+							<div class="slide-filter">
+								<ul>
+								<li><a href="#">General Comment</a></li>
+								<li><a href="#">Favorite</a></li>
+								<li><a href="#">Memory</a></li>
+								<li><a href="#">Interpretation</a></li>
+								<li><a href="#">Song Meaning</a></li>
+								<li><a href="#">My Opinion</a></li>
+								<li><a href="#">Translation</a></li>
+								<li><a href="#">Link(s)</a></li>
+								<li class="last"><a href="#">Song Comparison</a></li>
+							</ul>
+							<div class="slide-nav-mobile">
+								<ul>
+									<li><a href="#">General Comment</a></li>
+									<li><a href="#">Favorite</a></li>
+									<li><a href="#">Memory</a></li>
+									<li><a href="#">Interpretation</a></li>
+									<li><a href="#">Song Meaning</a></li>
+								</ul>
+								<ul>
+									<li><a href="#">My Opinion</a></li>
+									<li><a href="#">Translation</a></li>
+									<li><a href="#">Link(s)</a></li>
+									<li class="last"><a href="#">Song Comparison</a></li>
+								</ul>
+							</div>
+							<div class="slide-nav-tablet">
+								<ul>
+									<li><a href="#">General Comment</a></li>
+									<li><a href="#">Favorite</a></li>
+									<li><a href="#">Memory</a></li>
+								</ul>
+								<ul>
+									<li><a href="#">Interpretation</a></li>
+									<li><a href="#">Song Meaning</a></li>
+									<li><a href="#">My Opinion</a></li>
+								</ul>
+								<ul>
+									<li><a href="#">Translation</a></li>
+									<li><a href="#">Link(s)</a></li>
+									<li class="last"><a href="#">Song Comparison</a></li>
+								</ul>
+							</div>
+							</div>
+							<div class="slide-sort">
+								<ul>
+									<li><a href="#">Highest Rated</a></li>
+									<li><a href="#">Oldest First</a></li>
+									<li class="active"><a href="#">Newest First</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</footer>
+				<div class="scrollable-area comments-area">
+					<div class="comments-list">
+						<article class="comments-item">
+							<div class="add-info">
+								<footer>
+									<strong class="title"><a href="#">postrchildon</a></strong>
+									<time class="date" datetime="2013-02-28">February 28, 2013</time>
+									<div class="links">
+										<a href="#">37 Replies</a> <span class="leave-reply">| <a href="#">Leave Your Reply</a></span>
+									</div>
+								</footer>
+								<div class="sub-title">
+									<span>General Comment:</span>
+									<a href="#" class="link-flag">flag</a>
+								</div>
+								<p>I feel this song is about a girl he perhaps feels connected with, by an umbilical cord that is invicible. Sort of like a <span class="hidden-mobile">soul mate/twin flame. Given that he's a Pisces and if you were to do <span class="hidden-tablet"> research on the Piscean sign and the Virgoian, you may find some more answers biblical but also stemming from</span></span>..... <a href="#">Expand Comment</a></p>
+							</div>
+							<div class="rating-holder">
+								<strong class="title"><span>Comment </span>Rating</strong>
+								<div class="count">+27</div>
+								<nav class="action">
+									<ul>
+										<li class="minus"><a href="#">-</a></li>
+										<li><a href="#">+</a></li>
+									</ul>
+								</nav>
+							</div>
+						</article>
+						<article class="comments-item">
+							<div class="add-info">
+								<footer>
+									<strong class="title"><a href="#">Seerealityon</a></strong>
+									<time class="date" datetime="2013-01-31">January 31, 2013</time>
+									<div class="links">
+										<span>No Replies</span> <span class="leave-reply">| <a href="#">Leave Your Reply</a></span>
+									</div>
+								</footer>
+								<div class="sub-title">
+									<span>My Interpretation:</span>
+									<a href="#" class="link-flag">flag</a>
+								</div>
+								<p>Ok, Want2Lrn has it right, at least I think.</p>
+								<p>Anna Molly is not <span class="hidden-mobile">talking about a person... think <span class="hidden-tablet"> about the way its sung. Its really meant to</span></span>..... <a href="#">Expand Comment</a></p>
+							</div>
+							<div class="rating-holder">
+								<strong class="title"><span>Comment </span>Rating</strong>
+								<div class="count">+3</div>
+								<nav class="action">
+									<ul>
+										<li class="minus"><a href="#">-</a></li>
+										<li><a href="#">+</a></li>
+									</ul>
+								</nav>
+							</div>
+						</article>
+						<article class="comments-item">
+							<div class="add-info">
+								<footer>
+									<strong class="title"><a href="#">Want2Lrn</a></strong>
+									<time class="date" datetime="2012-09-20">September 20, 2012</time>
+									<div class="links">
+										<a href="#">5 Replies</a> <span class="leave-reply">| <a href="#">Leave Your Reply</a></span>
+									</div>
+								</footer>
+								<div class="sub-title">
+									<span>General Comment:</span>
+									<a href="#" class="link-flag">flag</a>
+								</div>
+								<p>How come I end up where I started (How come I will die just as I was dead before I was born) How come I end <span class="hidden-mobile">up where I belong (How come I am alive now) Won't take my eyes off the ball again (I will try a realise what life is about and get away from the systems of society) You reel me out then</span>..... <a href="#">Expand Comment</a></p>
+							</div>
+							<div class="rating-holder">
+								<strong class="title"><span>Comment </span>Rating</strong>
+								<div class="count">+11</div>
+								<nav class="action">
+									<ul>
+										<li class="minus"><a href="#">-</a></li>
+										<li><a href="#">+</a></li>
+									</ul>
+								</nav>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer id="footer">
+			<div class="footer-holder">
+				<div class="footer-info">
+					<p>Non-lyrical content copyright 1999-2014 SongMeanings <br>In partnership with <a href="#">ToneMedia</a></p>
+				</div>
+				<ul class="social-networks">
+					<li><a href="#" class="facebook">Facebook</a></li>
+					<li><a href="#" class="twitter">Twitter</a></li>
+					<li><a href="#" class="google">GooglePlus</a></li>
+					<li><a href="#" class="youtube">YouTube</a></li>
+				</ul>
+			</div>
+			<div class="footer-section">
+				<a href="#wrapper" class="back-top">Back to Top</a>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
